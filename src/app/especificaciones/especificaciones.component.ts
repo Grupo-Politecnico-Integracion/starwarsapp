e47e04f9e87c37ed96db2294a51cd49c36f4edb4
Mauricio <cesalcedo1@poligran.edu.co>
2020-04-11 17:18:48 -0500

    Nuevas dependencias

--- src/app/especificaciones/especificaciones.component.ts
+++ src/app/especificaciones/especificaciones.component.ts
@@ -3,13 +3,16 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-especificaciones',
   templateUrl: './especificaciones.component.html',
-  styleUrls: ['./especificaciones.component.css']
+   styleUrls: ['./especificaciones.component.css']
+  
 })
 export class EspecificacionesComponent implements OnInit {
 
   constructor() { }
 
   ngOnInit(): void {
+    
   }
+
 
 }
