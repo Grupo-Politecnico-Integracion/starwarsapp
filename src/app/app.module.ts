bab90695b837c19615b6eb90bcf0fc6fd3b71677
PLeal1266 <57365947+PLeal1266@users.noreply.github.com>
2020-04-11 16:55:11 -0500

    Footer
    Se crea componente components/footer-principal
    Se actualiza referencia del icono de pestaña en el archivo de referencias.txt
    Se crea el footer.
    Se agrega imagen a la ruta assets/images usada como icono de pestaña del navegador.
    Se actualiza archivo index ubicando respectivos header y footer, No son funcionales aun.
    Pendiente invocarlos directamente desde el Index y no desde componente app.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderPrincipalComponent } from './components/header-principal/header-principal.component';
+import { FooterPrincipalComponent } from './components/footer-principal/footer-principal.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderPrincipalComponent
+    HeaderPrincipalComponent,
+    FooterPrincipalComponent
   ],
   imports: [
     BrowserModule,
