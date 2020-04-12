441aba0bee4a82f4c9d6676d35b6aaef5ed19ff8
juriosro <57241946+juriosro@users.noreply.github.com>
2020-04-11 21:28:42 -0500

    Incluir imagenes con efectos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderPrincipalComponent } from './components/header-principal/header-principal.component';
+import { CatalogosComponent } from './components/catalogos/catalogos.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderPrincipalComponent,
+    CatalogosComponent
   ],
   imports: [
     BrowserModule,
