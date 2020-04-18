109496bdc0037e221c59c27c0bcc210ef81cdc5e
Mauricio <cesalcedo1@poligran.edu.co>
2020-04-18 15:14:28 -0500

    Actualización Módulo componentes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,17 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { CatalogosComponent } from './components/catalogos/catalogos.component';
 
-// Add CMS 18.04.2020 Anexo ruta para especificaciones
-import { EspecificacionesComponent } from './especificaciones/especificaciones.component';
-// Fin Add Anexo ruta para especificaciones CMS 18.04.2020
+
 
 
 const routes: Routes = [
   { path: 'films', component: CatalogosComponent},
 
-  // Add PATH para especificaciones CMS 18.04.2020
-  { path: 'descripFilms', component: EspecificacionesComponent},
-  // Fin Add PATH para especificaciones CMS 18.04.2020 
 ];
 
 @NgModule({
