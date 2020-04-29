58b652d9f1b9516d8448c62c7e8b89b9350e897e
Lorena Forero <yeforero3@poligran.edu.co>
2020-04-28 23:14:55 -0500

    lectura del API y arreglo al menú

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,23 +1,28 @@
 import { Component } from '@angular/core';
 // Importo el servicio CMS 20.04.2020
 import { StarwarsApiService } from './starwars-api.service';
+import { HeaderPrincipalComponent } from './components/header-principal/header-principal.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   title = 'stars';
-
+  url: string; // Variable URL que consulto de la lista de ids del menu LorenaF 28.04.2020
   // Implementación de la appi CMS 20.04.2020
   stars: any[] = [];
-
+  
   constructor(protected userService: StarwarsApiService) {
+    
   }
 
   ngOnInit() {
-    this.userService.getStarwarsApi()
+    let header = new HeaderPrincipalComponent();//Creo objeto de la clase de header-component
+    this.url = header.getUrl();// Traigo URL de la lista de la clase de header-component
+    this.userService.getStarwarsApi(this.url)//Utilizo la URL en el metodo que consume la API
     .subscribe(
       (data) => { // Success
         this.stars = data['results'];
@@ -27,4 +32,6 @@ export class AppComponent {
       }
     );
   }
+
+
 }
--- src/app/components/header-principal/header-principal.component.ts
+++ src/app/components/header-principal/header-principal.component.ts
@@ -7,9 +7,50 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderPrincipalComponent implements OnInit {
 
+//LorenaF.lista de ids para las opciones del menù 28.04.2020
+  menu: any[] = [];
+  url = 'http://swapi.py4e.com/api/people/?results=100';
   constructor() { }
 
   ngOnInit(): void {
+    //LorenaF.lleno lista de ids para las opciones del menù 28.04.2020
+    this.menu = [
+      {
+        "id": "1",//Peliculas
+        "url": "",
+      },
+      {
+        "id": "2",//Planetas
+        "url": "",
+      },
+      {
+        "id": "3",//Personajes
+        "url": "http://swapi.py4e.com/api/people/?results=100",
+      },
+      {
+        "id": "4",//Especies
+        "url": "",
+      },
+      {
+        "id": "5",//Vehiculos
+        "url": "",
+      },
+      {
+        "id": "6",//Naves
+        "url": "",
+      }
+    ];
+  }
+
+  getUrl () {
+    // for (let index = 0; index < this.menu.length; index++) {
+    //   if () {
+        
+    //   }
+      
+    // }
+    
+    return this.url;
   }
 
 }
