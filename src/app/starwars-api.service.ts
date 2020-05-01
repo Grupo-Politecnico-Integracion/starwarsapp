6322809937dff84396a429bb9db87cb2329cc856
Mauricio <cesalcedo1@poligran.edu.co>
2020-05-01 10:29:37 -0500

    Loop para films

--- src/app/starwars-api.service.ts
+++ src/app/starwars-api.service.ts
@@ -18,7 +18,8 @@ export class StarwarsApiService {
   // Metodo que retorna la api CMS 20.04.2020
   getStarwarsApi() {
     // return this.http.get('http://swapi.py4e.com/api/?results=5'); Comentado para prueba de nuevo link 20.04.2020
-    return this.http.get('http://swapi.py4e.com/api/people/?results=100');
+    // return this.http.get('http://swapi.py4e.com/api/people/?results=100');
+    return this.http.get('http://swapi.py4e.com/api/films/?results=100');
   }
 
 }
